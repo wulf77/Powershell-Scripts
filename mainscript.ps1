7df6bd438ff2a517874094d7acdb3f54bbb16722
sydney.s <145722632+wulf77@users.noreply.github.com>
2023-11-27 20:51:33 -0600

    Update mainscript.ps1
    Changed several functions for the main script including: 
    -Updated the password change function
    -Deleted parts of the audit policy editor which didn't work.
    -Removed other various redundant or non-working parts of the script

--- mainscript.ps1
+++ mainscript.ps1
@@ -1,13 +1,17 @@
-# Edits needed: line 7(remove/add exit command to protect yourself), line 15 (username), line 119 (for uninstall package names)
+#
+# In order to run the script, you will need to run the command Set-ExecutionPolicy RemoteSigned
+# Edits needed: line 7(remove exit command), line 15 (username), line 119 (for uninstall package names)
 # For user comparison and deletion, run other script separately
 # For more difficult problems, try downloading Win. Sysinternal Suite
-# In order to run the script, you will need to run the command Set-ExecutionPolicy RemoteSigned
-# Meant to be run with the usercompare script found in my Powershell repository
+
+
+
+exit
 
 echo "Job 1) ReadMe and forensics questions"
 pause
 
-# The following opens a link to the readme
+# echo "The following opens a link to the readme:"
 # (Readme link here)
 # (Scoring report link here) 
 pause
@@ -69,33 +73,23 @@ cls
 # Disables the guest account 
 net user Guest /active no 
 
-# Changes all passwords to Cyb3rP@triot23! except for the specified user(s).
-$newPassword = ConvertTo-SecureString -String "Cyb3rP@triot23!" -AsPlainText -Force
-# Get a list of all user accounts on the computer
-$users = Get-LocalUser
-
-# Loop through each user and change their password
-foreach ($user in $users) {
-    # Skip the user named "eleven"
-    if ($user.Name -eq "eleven") {
-        Write-Host "Skipping user: $($user.Name)"
-        continue
-    }
-
-    # Change the password for the user
-    try {
-        Set-LocalUser -Name $user.Name -Password $newPassword
-        Write-Host "Password changed for user: $($user.Name)"
-    } catch {
-        Write-Host "Error changing password for user: $($user.Name) - $_.Exception.Message"
-    }
-}
+# Displays the current user's username in prep for password change
+echo "Please ensure that you do not change the main account password."
+pause 
+whoami
+pause
 
 # Define the new password
 $newPassword = "Cyb3rP@triot23!"
 
-# Specify the usernames to exclude from password change
-$excludedUsers = "eleven"
+# Prompt the user to enter the names of users to skip password changes
+$skipUsers = Read-Host "Enter the name(s) of the user(s) to skip password changes (comma-separated). Press Enter to skip entering a username."
+
+# Split the input into an array of usernames
+$excludedUsers = @()
+if ($skipUsers -ne "") {
+    $excludedUsers = $skipUsers -split ','
+}
 
 # Get a list of all user accounts on the computer
 $users = Get-WmiObject -Class Win32_UserAccount
@@ -106,7 +100,7 @@ foreach ($user in $users) {
 
     # Skip the excluded users
     if ($excludedUsers -contains $username) {
-        Write-Host "Skipping user: $username"
+        Write-Host "Skipping password change for user: $username"
         continue
     }
 
@@ -171,130 +165,57 @@ pause
 cls
 
 echo "Job 3) Audit policies"
-# Changes the audit policy in control panel to success, failure.
-# Define a list of audit categories to configure (you can add or remove categories as needed)
-
-# NOTE: the auditpol editors do not work as intended yet
-
-# Changes the account policy settings and lockout durations. 
-# Define the path to the security configuration template file
-$securityConfigPath = "C:\Path\To\AccountPolicy.inf"
-
-# Create the security configuration template
-$securityConfig = @"
-[Unicode]
-Unicode=yes
-[Version]
-signature="$CHICAGO$"
-Revision=1
-[System Access]
-MinimumPasswordAge = 30
-MaximumPasswordAge = 90
-MinimumPasswordLength = 10
-PasswordComplexity = 1
-PasswordHistorySize = 5
-[Lockout Policy]
-LockoutBadCount = 10
-LockoutDuration = 30
-ResetLockoutCount = 30
-"@
-# Save the security configuration to a file
-$securityConfig | Out-File -FilePath $securityConfigPath -Encoding Unicode
-# Apply the security configuration using secedit.exe
-secedit.exe /configure /db "C:\Windows\Security\local.sdb" /cfg $securityConfigPath /areas SECURITYPOLICY
-# Remove the temporary security configuration file
-Remove-Item -Path $securityConfigPath -Force
-
-# NOTE that the above may not work. here is an alt. option that is far more likely to succeed. 
-echo "Running an alternative audit policy modifier." 
-pause
-# Define the audit and password policy settings
-$policySettings = @{
-    "AuditSystemEvents" = "3:0:0"
-    "AuditLogonEvents" = "2:0:0"
-    "AuditObjectAccess" = "3:0:0"
-    "AuditPrivilegeUse" = "3:0:0"
-    "AuditPolicyChange" = "3:0:0"
-    "AuditAccountManage" = "3:0:0"
-    "AuditProcessTracking" = "3:0:0"
-    "AuditDSAccess" = "3:0:0"
-    "AuditAccountLogon" = "2:0:0"
-    "MinimumPasswordAge" = "0,1,0"
-    "MaximumPasswordAge" = "0,90,0"
-    "MinimumPasswordLength" = "0,8,0"
-    "PasswordComplexity" = "0,1,0"
-    "PasswordHistorySize" = "0,5,0"
-    "LockoutBadCount" = "0,10,0"
-    "EnableGuestAccount" = "0,0,0"
-}
-
-# Export the current local security policy settings to a temporary file
-$exportPath = "$env:TEMP\local.cfg"
-secedit /export /cfg $exportPath > $null
-
-# Import the existing settings from the temporary file
-$existingSettings = Get-Content $exportPath -Raw
-
-# Update the audit and password policy settings in the exported configuration
-foreach ($key in $policySettings.Keys) {
-    $existingSettings = $existingSettings -replace "$key = .+", "$key = $($policySettings[$key])"
-}
-
-# Write the updated settings back to the temporary file
-$existingSettings | Set-Content $exportPath
-
-# Apply the changes to the security policy
-secedit /configure /db $env:windir\security\local.sdb /cfg $exportPath /areas SECURITYPOLICY
-cls
-
-echo "Third time is the charm- last attempt at success/failure auditing." 
-pause
-
+pause 
 <#
 .SYNOPSIS
-Sets and defines the audit settings in Windows 10 to success and failure for all audit settings in Local Security Policy > Local Policies.
+Changes the local security policy audit settings and account lockout threshold.
 
 .DESCRIPTION
-This function uses the Group Policy cmdlets in PowerShell to configure the audit settings in Windows 10. It sets the audit settings to success and failure for all audit categories in Local Security Policy > Local Policies.
+This function modifies the local group policy object to set the audit settings for success and failure, 
+and sets the account lockout threshold to the specified value.
 
-.NOTES
-- This function requires administrative privileges to modify the Group Policy settings.
-- This function is specifically designed for Windows 10.
+.PARAMETER AccountLockoutThreshold
+The value to set for the account lockout threshold.
 
 .EXAMPLE
-Set-AuditSettings
-Sets and defines the audit settings in Windows 10 to success and failure for all audit settings in Local Security Policy > Local Policies.
+Set-SecurityPolicy -AccountLockoutThreshold 10
+Sets the audit settings for success and failure, and sets the account lockout threshold to 10.
 #>
-function Set-AuditSettings {
-    # Import the Group Policy module
-    Import-Module GroupPolicy
+function Set-SecurityPolicy {
+    param (
+        [int]$AccountLockoutThreshold
+    )
 
-    # Get the Local Security Policy object
-    $localSecurityPolicy = Get-GPObject -All | Where-Object { $_.DisplayName -eq "Local Security Policy" }
+    try {
+        # Set the audit settings for success and failure
+        secedit.exe /export /cfg "$env:TEMP\security.cfg"
+        (Get-Content "$env:TEMP\security.cfg") | ForEach-Object {
+            $_ -replace "AuditPolicySuccess = 0", "AuditPolicySuccess = 1" `
+               -replace "AuditPolicyFailure = 0", "AuditPolicyFailure = 1"
+        } | Set-Content "$env:TEMP\security.cfg"
+        secedit.exe /configure /db "$env:TEMP\secedit.sdb" /cfg "$env:TEMP\security.cfg" /areas SECURITYPOLICY
 
-    # Get the Local Policies object
-    $localPolicies = Get-GPRegistryValue -Guid $localSecurityPolicy.Id -Key "Machine\Software\Microsoft\Windows\CurrentVersion\Policies\System\Audit"
+        # Set the account lockout threshold
+        $policyPath = "HKLM:\System\CurrentControlSet\Services\Netlogon\Parameters"
+        Set-ItemProperty -Path $policyPath -Name "LockoutThreshold" -Value $AccountLockoutThreshold
 
-    # Set the audit settings to success and failure for all audit categories
-    foreach ($policy in $localPolicies) {
-        $policy.Value = 3  # Set the value to 3 to enable success and failure auditing
+        Write-Output "Security policy settings updated successfully."
     }
-
-    # Save the changes to the Local Security Policy
-    Set-GPRegistryValue -Guid $localSecurityPolicy.Id -Key "Machine\Software\Microsoft\Windows\CurrentVersion\Policies\System\Audit" -Value $localPolicies
-
-    # Refresh the Group Policy settings
-    gpupdate /force
+    catch {
+        Write-Error "Failed to update security policy settings: $_"
+    }
 }
 
-# Usage example for the Set-AuditSettings function
-Set-AuditSettings
+# Usage example for the Set-SecurityPolicy function
 
-echo "This concludes the editing of the audit policy and password settings. To check manually, hit Enter"
+Set-SecurityPolicy -AccountLockoutThreshold 10
+
+#Manual because the auditpol editors had bugs and only dragged the script down. Will continue to work on a solution."
+echo "Check the audit policy settings in local secpol manually."
+sleep 2 
+echo "Check the Local Sec Pol settings manually. (Hit Win+R and type secpol.)"
 pause
-auditpol
-pause
-cls
+cls 
 
 echo "Job 4) File system security"
 pause
@@ -321,8 +242,7 @@ Get-ChildItem -Path C:\Users -Recurse -File -Filter *.bat
 Get-ChildItem -Path C:\Users -Recurse -File -Filter *.jpeg
 pause 
 cls
-pause 
-cls
+
 
 echo "Stop file sharing."
 net stop lanmanserver
@@ -359,6 +279,7 @@ start-sleep -seconds 3
 appwiz.cpl
 pause
 
+echo "Download and launch Windows Sysinternal Suites"
 echo "Download autoruns program to see programs that start by themselves."
 pause
 https://docs.microsoft.com/en-us/sysinternals/downloads/autoruns
@@ -393,6 +314,10 @@ echo "paste this link into your browser for the direct web page to the current v
 write-host "https://github.com/wulf77/Powershell-Scripts/blob/main/usercompareV2.ps1"
 pause 
 # Include instructions for both this script and the user compare, and how to run them in a readme file on github
+
+
+
+
 
 
 
